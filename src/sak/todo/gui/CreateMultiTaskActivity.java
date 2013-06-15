89ade22b124cfa815249905e160e740c3f0df32c
kareem tarek selim <kareem.csd@gmail.com>
2013-06-15 20:05:10 +0300

    kareem tarek update

--- src/sak/todo/gui/CreateMultiTaskActivity.java
+++ src/sak/todo/gui/CreateMultiTaskActivity.java
@@ -4,19 +4,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Vector;
 
 import sak.todo.database.Task;
 import sak.todo.database.TasksIterator;
 import sak.todo.gui.schedules.SchedulesActivity;
 import taskero.learner.Preference_Learner;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.Dialog;
+import android.app.FragmentTransaction;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.LayoutInflater;
@@ -26,10 +31,17 @@ import android.view.View.DragShadowBuilder;
 import android.view.View.OnClickListener;
 import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,28 +49,133 @@ import com.googlecode.android.widgets.DateSlider.DateSlider;
 import com.googlecode.android.widgets.DateSlider.DateTimeSlider;
 import com.learner.svm.SVMAdapter;
 
-import cr.Constraint;
 import cr.Interval;
 import cr.Reasoner;
 import cr.STPNotConnsistentException;
 
-public class CreateMultiTaskActivity extends Activity {
+public class CreateMultiTaskActivity extends Activity implements TabListener {
 	int i = 1;
 	LinearLayout listofAddedTasks;
 	LinearLayout listofConstraintBefore;
 	LinearLayout listofConstraintAfter;
 	ArrayList<Task> addedTasks;
+	boolean listofAddedTaskAppears;
 	public static final String PREFS_NAME = "MyPrefsFile";
 	// ArrayList<Constraint> constraints;
 	public SharedPreferences sharedPrefs;
-
+	private int screenWidth;
+	private int screenHeight;
+	static int priority;
+	
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		if(listofAddedTaskAppears){
+			
+			animateBack();
+			
+		}
+		else super.onBackPressed();
+	}
+	boolean before=false;
+	private void animateBefore(boolean before){
+		this.before=before;
+		Log.d("debug", "anim");
+		TranslateAnimation anim=new TranslateAnimation(100,0,0,0);
+		
+		anim.setDuration(500);
+		AnimationSet logoAnimSet = new AnimationSet(true);
+		logoAnimSet.setInterpolator(new LinearInterpolator());
+		logoAnimSet.addAnimation(anim);
+		logoAnimSet.setFillAfter(true);
+		findViewById(R.id.ScrollViewParent).setAlpha(0.4f);
+		estimateView.setEnabled(false);
+		prioritylow.setClickable(false);
+		prioritymedium.setClickable(false);
+		priorityhigh.setClickable(false);
+		bodyView.setEnabled(false);
+		deadLineView.setClickable(false);
+		dueDateView.setClickable(false);
+		CreateMultiTaskActivity.this.findViewById(R.id.SaveAll).setVisibility(View.GONE);
+		CreateMultiTaskActivity.this.findViewById(R.id.AddTask).setVisibility(View.GONE);
+		CreateMultiTaskActivity.this.findViewById(R.id.AddTask).setEnabled(false);
+		
+		findViewById(R.id.ScrollViewListofAddedTasks).startAnimation(logoAnimSet);
+		findViewById(R.id.ScrollViewListofAddedTasks).setVisibility(View.VISIBLE);
+		findViewById(R.id.ScrollViewListofAddedTasks).setClickable(true);
+		
+		listofAddedTaskAppears=true;
+	}
+	private void animateBack() {
+		// TODO Auto-generated method stub
+		Log.d("debug", "here animate back");
+		TranslateAnimation anim=new TranslateAnimation(0,100,0,0);
+		
+		anim.setDuration(500);
+		AnimationSet logoAnimSet = new AnimationSet(true);
+		logoAnimSet.setInterpolator(new LinearInterpolator());
+		logoAnimSet.addAnimation(anim);
+		logoAnimSet.setFillAfter(true);
+		findViewById(R.id.ScrollViewParent).setAlpha(1f);
+		bodyView.setEnabled(true);
+		
+		estimateView.setEnabled(true);
+		
+		priorityhigh.setClickable(true);
+		prioritylow.setClickable(true);
+		prioritymedium.setClickable(true);
+		deadLineView.setClickable(true);
+		dueDateView.setClickable(true);
+		CreateMultiTaskActivity.this.findViewById(R.id.SaveAll).setVisibility(View.VISIBLE);
+		CreateMultiTaskActivity.this.findViewById(R.id.AddTask).setVisibility(View.VISIBLE);
+		CreateMultiTaskActivity.this.findViewById(R.id.AddTask).setEnabled(true);
+		findViewById(R.id.ScrollViewListofAddedTasks).setVisibility(View.GONE);
+		findViewById(R.id.ScrollViewListofAddedTasks).setClickable(false);
+		CreateMultiTaskActivity.this.findViewById(R.id.SaveAll).bringToFront();
+		
+		
+		findViewById(R.id.ScrollViewListofAddedTasks).startAnimation(logoAnimSet);
+		View v = findViewById(R.id.ScrollViewListofAddedTasks);
+		((RelativeLayout)findViewById(R.id.RelativeLayout1)).removeView(v);
+		((RelativeLayout)findViewById(R.id.RelativeLayout1)).addView(v);
+		listofAddedTaskAppears = false;
+		
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.create_multi_task);
 		
-		bodyView = (EditText) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.taskBody);
-		priorityView = (RatingBar) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.ratingBar1);
+		DisplayMetrics metrics = getApplicationContext().getResources().getDisplayMetrics();
+		screenWidth = metrics.widthPixels;
+		screenHeight = metrics.heightPixels;
+		
+		// requesting action bar feature
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		
+		setContentView(R.layout.create_multi_task);
+		// setup action bar for tabs
+	    ActionBar actionBar = getActionBar();
+	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	    actionBar.setDisplayShowTitleEnabled(false);
+	    actionBar.setHomeButtonEnabled(false);
+	    actionBar.setDisplayShowTitleEnabled(false);
+	    actionBar.setDisplayShowHomeEnabled(false);
+	    actionBar.setStackedBackgroundDrawable(new ColorDrawable(R.color.tabsColor));
+		
+		
+		Tab tab1= actionBar.newTab().setText("Quick Task").setTabListener(this);
+		Tab tab2= actionBar.newTab().setText("Single Task").setTabListener(this);
+		Tab tab3= actionBar.newTab().setText("Multi task").setTabListener(this);
+		actionBar.addTab(tab1);
+		actionBar.addTab(tab2);
+		actionBar.addTab(tab3);
+		
+		
+		
+		bodyView = (EditText) this.findViewById(R.id.RelativeLayout1).findViewById(R.id.ScrollViewParent).findViewById(R.id.LinearLayoutTaskcrud).findViewById(R.id.taskBody);
+		priorityhigh = (Button) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.rate_high);
+		prioritylow = (Button) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.rate_low);
+		prioritymedium = (Button) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.rate_medium);
 		estimateView = (EditText) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.editText1);
 		deadLineView = (Button) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.deadline);
 		dueDateView = (Button) this.findViewById(R.id.ScrollViewParent).findViewById(R.id.duedate);
@@ -68,27 +185,109 @@ public class CreateMultiTaskActivity extends Activity {
 		if (!sharedPrefs.contains("numberOfRuns"))
 			sharedPrefs.edit().putInt("numberOfRuns", 0).commit();
 
-		listofAddedTasks = ((LinearLayout) findViewById(R.id.ListofAddedTasks));
+		listofAddedTasks = ((LinearLayout)findViewById(R.id.RelativeLayout1).findViewById(R.id.ScrollViewListofAddedTasks).findViewById(R.id.ListofAddedTasks));
 		listofConstraintBefore = ((LinearLayout) findViewById(R.id.ConstraintLayoutBefore));
 		listofConstraintAfter = ((LinearLayout) findViewById(R.id.ConstraintLayoutAfter));
 		
-		TextView taskBefore = (TextView) LayoutInflater.from(
-				CreateMultiTaskActivity.this).inflate(R.drawable.task,
-				null);
-		TextView taskAfter = (TextView) LayoutInflater.from(
-				CreateMultiTaskActivity.this).inflate(R.drawable.task,
-				null);
+		priority=0;
+		priorityhigh.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				priority=3;
+				((TextView)findViewById(R.id.Rate_Statue)).setText("Urgent!");
+			}
+		});
+		prioritylow.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				priority=1;
+				((TextView)findViewById(R.id.Rate_Statue)).setText("low");
+			}
+		});
+		prioritymedium.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				priority=2;
+				((TextView)findViewById(R.id.Rate_Statue)).setText("medium");
+			}
+		});
 		
-		taskBefore.setBackgroundResource(R.drawable.shape_clicked);
-		taskAfter.setBackgroundResource(R.drawable.shape_clicked);
-		taskBefore.setText(" ADD ");
-		taskAfter.setText(" ADD ");
-		
-		listofConstraintBefore.addView(taskBefore);
-		listofConstraintAfter.addView(taskAfter);
+		findViewById(R.id.ScrollViewListofAddedTasks).setOnTouchListener(new OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				Log.d("debug", "he is aliveeeeee");
+				return false;
+			}
+		});
+		findViewById(R.id.ScrollViewListofAddedTasks).setVisibility(View.GONE);
+		findViewById(R.id.ScrollViewListofAddedTasks).setClickable(false);
+		findViewById(R.id.ScrollViewParent).setOnTouchListener(new OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				
+				
+				return false;
+			}
+		});
+		findViewById(R.id.ConstraintLayoutAfter).findViewById(R.id.ButtonAddTaskAfter).setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				animateBefore(false);
+			}
 
-		taskBefore.setOnDragListener(new constraintDragListner(true));
-		taskAfter.setOnDragListener(new constraintDragListner(false));
+		});
+		findViewById(R.id.ConstraintLayoutBefore).findViewById(R.id.ButtonAddTaskBefore).setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				animateBefore(true);
+			}
+		});
+		
+
+//		findViewById(R.id.ScrollViewParent).setOnTouchListener(new OnTouchListener() {
+//			
+//			public boolean onTouch(View v, MotionEvent event) {
+//				// TODO Auto-generated method stub
+//				if(listofAddedTaskAppears)listofAddedTasks.performClick();
+//				return false;
+//			}
+//		
+//		});
+//		
+//		listofAddedTasks.setOnClickListener(new OnClickListener() {
+//			
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				Log.d("debug", "listof added");
+//				v.performClick();
+//			}
+//		});
+		
+		
+		final View activityRootView = findViewById(R.id.RelativeLayout1);
+		activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+		    public void onGlobalLayout() {
+		        int heightDiff = activityRootView.getRootView().getHeight() - activityRootView.getHeight();
+		        if (heightDiff > 100) { // if more than 100 pixels, its probably a keyboard...
+		        	CreateMultiTaskActivity.this.findViewById(R.id.SaveAll).setVisibility(View.INVISIBLE);
+					CreateMultiTaskActivity.this.findViewById(R.id.AddTask).setVisibility(View.INVISIBLE);
+		        }
+		        else
+		        {
+		        	CreateMultiTaskActivity.this.findViewById(R.id.SaveAll).setVisibility(View.VISIBLE);
+					CreateMultiTaskActivity.this.findViewById(R.id.AddTask).setVisibility(View.VISIBLE);
+		        }
+		     }
+		});
+		
 
 		addedTasks = new ArrayList<Task>();
 		addedTasks.add(Task.NULLTASK);
@@ -166,7 +365,7 @@ public class CreateMultiTaskActivity extends Activity {
 				//
 				String body = bodyView.getText().toString();
 				task.setText(body);
-				int priority = (int) (priorityView.getRating() * 2);
+				int priority = CreateMultiTaskActivity.priority;
 				String _estimate = estimateView.getText().toString();
 
 				if (body.isEmpty()) {
@@ -211,8 +410,8 @@ public class CreateMultiTaskActivity extends Activity {
 				addedTasks.add(parentTask);
 				parentTask = new Task();
 				bodyView.setText("");
-				priorityView.setRating(0);
-
+				priority=0;
+				((TextView)findViewById(R.id.Rate_Statue)).setText("");
 				estimateView.setText("");
 				for (int i = 1; i < listofConstraintAfter.getChildCount() - 1; i++) {
 					listofConstraintAfter.removeViewAt(i + 1);
@@ -229,58 +428,110 @@ public class CreateMultiTaskActivity extends Activity {
 				Toast.makeText(CreateMultiTaskActivity.this,
 						"Saved successfuly.", Toast.LENGTH_SHORT).show();
 
-				task.setOnTouchListener(new OnTouchListener() {
-
-					public boolean onTouch(View v, MotionEvent event) {
-
-						if (event.getAction() == MotionEvent.ACTION_DOWN) {
-							ClipData data = ClipData.newPlainText("", "");
-
-							DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-									v);
-							v.startDrag(data, shadowBuilder, v, 0);
-							// v.setVisibility(View.INVISIBLE);
-							v.setBackgroundResource(R.drawable.shape_clicked);
-							return true;
-						} else {
-							return false;
-						}
-					}
-				});
-				task.setOnDragListener(new OnDragListener() {
-
-					public boolean onDrag(View v, DragEvent event) {
+				task.bringToFront();
+				
+				task.setOnClickListener(new OnClickListener() {
+					
+					public void onClick(View v) {
 						// TODO Auto-generated method stub
 
-						int action = event.getAction();
-						switch (event.getAction()) {
-						case DragEvent.ACTION_DRAG_STARTED:
-							// Do nothing
-							break;
-						case DragEvent.ACTION_DRAG_ENTERED:
+						ClipData data = ClipData.newPlainText("", "");
+						Log.d("debug", "hereeee");
+						DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+								v);
+//						v.startDrag(data, shadowBuilder, v, 0);
+						// v.setVisibility(View.INVISIBLE);
+//						v.setBackgroundResource(R.drawable.shape_clicked);
+						animateBack();
+						View view = v;
+						TextView task = (TextView) LayoutInflater.from(
+								CreateMultiTaskActivity.this).inflate(
+								R.drawable.task, null);
+						;
+						
+						task=new TextView(CreateMultiTaskActivity.this);
+						
+						task.setText("Task: "+((TextView) view).getText());
+						
+						task.setTextSize(20);
+						task.setPadding(30, 0, 0, 0);
+						
+						TextView t = new TextView(CreateMultiTaskActivity.this);
+						t.setText("  ");
+						t.setTextSize(20);
 
-							// v.setBackgroundResource(R.drawable.shape_clicked);
-							break;
-						case DragEvent.ACTION_DRAG_EXITED:
-							// v.setBackgroundResource(R.drawable.shape_clicked);
+						view.setVisibility(View.VISIBLE);
 
-							break;
-						case DragEvent.ACTION_DROP:
-							// Dropped, reassign View to ViewGroup
-							//
-							v.setBackgroundResource(R.drawable.shape);
-							// ((TableLayout)findViewById(R.id.ListofAddedTasks)).removeView(task);
-							// ((TableLayout)findViewById(R.id.ListofAddedTasks)).addView(task);
-
-							break;
-						case DragEvent.ACTION_DRAG_ENDED:
-							v.setBackgroundResource(R.drawable.shape);
-						default:
-							break;
+						if(before){
+							((LinearLayout)findViewById(R.id.ConstraintLayoutBefore)).addView(t);
+							((LinearLayout)findViewById(R.id.ConstraintLayoutBefore)).addView(task);
+							
 						}
-						return true;
+						else{
+							((LinearLayout)findViewById(R.id.ConstraintLayoutAfter)).addView(t);
+							((LinearLayout)findViewById(R.id.ConstraintLayoutAfter)).addView(task);
+							
+						}
+						// constraint creation:
+
+						Task t1 = null;
+						for (int i = 1; i < addedTasks.size(); i++) {
+							if (addedTasks.get(i).body.equals(task.getText() + "")) {
+								t1 = addedTasks.get(i);
+								Log.d("debug", "Constraint on drag task body: "
+										+ t1.body);
+							}
+						}
+
+						if (before) {
+							parentTask.addAfter(t1, new Interval(0, Reasoner.INFINITY));
+						} else {
+							parentTask
+									.addBefore(t1, new Interval(0, Reasoner.INFINITY));
+						}
+						
+
 					}
 				});
+					
+					
+				
+					
+			
+//				task.setOnDragListener(new OnDragListener() {
+//
+//					public boolean onDrag(View v, DragEvent event) {
+//						// TODO Auto-generated method stub
+//
+//						int action = event.getAction();
+//						switch (event.getAction()) {
+//						case DragEvent.ACTION_DRAG_STARTED:
+//							// Do nothing
+//							break;
+//						case DragEvent.ACTION_DRAG_ENTERED:
+//
+//							// v.setBackgroundResource(R.drawable.shape_clicked);
+//							break;
+//						case DragEvent.ACTION_DRAG_EXITED:
+//							// v.setBackgroundResource(R.drawable.shape_clicked);
+//
+//							break;
+//						case DragEvent.ACTION_DROP:
+//							// Dropped, reassign View to ViewGroup
+//							//
+//							v.setBackgroundResource(R.drawable.shape);
+//							// ((TableLayout)findViewById(R.id.ListofAddedTasks)).removeView(task);
+//							// ((TableLayout)findViewById(R.id.ListofAddedTasks)).addView(task);
+//
+//							break;
+//						case DragEvent.ACTION_DRAG_ENDED:
+//							v.setBackgroundResource(R.drawable.shape);
+//						default:
+//							break;
+//						}
+//						return true;
+//					}
+//				});
 			}
 		});
 	}
@@ -374,7 +625,7 @@ public class CreateMultiTaskActivity extends Activity {
 
 	private Task parentTask;
 	private EditText bodyView;
-	private RatingBar priorityView;
+	private Button prioritylow,priorityhigh,prioritymedium;
 	private EditText estimateView;
 	private Button deadLineView;
 	private Button dueDateView;
@@ -417,6 +668,18 @@ public class CreateMultiTaskActivity extends Activity {
 	public void showDateDialog(View view) {
 		dateText = (Button) view;
 		showDialog(DateSlider.DATETIMESELECTOR_ID);
+	}
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
